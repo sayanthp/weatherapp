86fefcec69922929c718a6dd58e2fb0c5adafaef
Psayanth <psayanth.p@gmail.com>
2022-06-16 00:13:50 +0530

    Added new component and a service

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import React,{Component} from 'react'
 import './App.css';
+import SearchBox from './SearchBox';
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
 
+    }
+  }
 
-
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <SearchBox />
+         
+  
+  
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
