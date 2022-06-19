f1f9b538d3e7fd9af8f4971c7423b7fbd5c41da5
Psayanth <psayanth.p@gmail.com>
2022-06-19 12:16:47 +0530

    api for fetching client ip added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
 import './App.css';
 import SearchBox from './SearchBox';
+import {getWeatherData} from './weatherService';
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-
+      location : ""
     }
   }
+
+  async componentDidMount() {
+    const response = await fetch('https://geolocation-db.com/json/');
+    const data = await response.json();
+    this.setState({ location: data.IPv4 });
+    getWeatherData(data.IPv4);
+}
 
   render(){
     return (
       <div>
-        <SearchBox />
+        <SearchBox location={this.state.location}/>
         
       </div>
     )
